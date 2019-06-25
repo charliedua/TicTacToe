70f6f21edf0f389f1c5dc80e2203332118d646ea
Anmol Dua <101983924@student.swin.edu.au>
2019-06-25 23:43:16 +1000

    AI vs AI for Tic Tac Toe

---
+++ TicTacToe/Game.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Game
+{
+    private int[] _data = new int[9];
+
+    public Game()
+    {
+        for (int i = 0; i < _data.Length; i++)
+        {
+            _data[i] = -1;
+        }
+    }
+
+    public void start()
+    {
+        int moveCount = 0;
+        int WhoseTurn = 1;
+        bool win = false;
+        int input = -1;
+        int whoWon = -1;
+        Console.WriteLine("Starting Game Now ...");
+        do
+        {
+            Console.WriteLine();
+            DrawBoard();
+            Console.WriteLine();
+            ShowMenu(WhoseTurn);
+
+            // Player Move
+            if (WhoseTurn == 1)
+            {
+                //try
+                //{
+                //    input = int.Parse(Console.ReadLine());
+                //}
+                //catch (Exception)
+                //{
+                //    input = -1;
+                //}
+                input = ComputerMove(WhoseTurn);
+            }
+            // Computer Move
+            else
+            {
+                input = ComputerMove(WhoseTurn);
+            }
+
+            if (input != -1)
+            {
+                bool validResult = false;
+                validResult = PutValue(input, WhoseTurn, _data);
+
+                if (!validResult)
+                {
+                    Console.WriteLine("\nInvalid Move!");
+                }
+                else
+                {
+                    WhoseTurn = WhoseTurn == 0 ? 1 : 0;
+                    moveCount++;
+                    whoWon = CheckWinCondition(_data);
+                    win = whoWon != -1;
+                }
+            }
+        }
+        while (moveCount < 9 && !win);
+
+        Console.WriteLine("Thanks for playing!\n");
+
+        DrawBoard();
+
+        Console.WriteLine("\n" + (win ? (XorYFromInt(whoWon) + " Won") : "DRAW"));
+    }
+
+    private int CheckWinCondition(int[] board)
+    {
+        for (int i = 0; i < 9;)
+        {
+            // Horizontal checking
+            if (board[i] != -1 && board[i] == board[i + 1] && board[i + 1] == board[i + 2])
+            {
+                return board[i];
+            }
+            i += 3;
+        }
+
+        // Vertical Checking
+
+        if (board[0] != -1 && board[0] == board[3] && board[3] == board[6])
+        {
+            return board[0];
+        }
+
+        if (board[1] != -1 && board[1] == board[4] && board[4] == board[7])
+        {
+            return board[1];
+        }
+
+        if (board[2] != -1 && board[2] == board[5] && board[5] == board[8])
+        {
+            return board[2];
+        }
+
+        // Diagonal Checking
+        if (board[0] != -1 && board[0] == board[4] && board[4] == board[8])
+        {
+            return board[0];
+        }
+
+        if (board[2] != -1 && board[2] == board[4] && board[4] == board[6])
+        {
+            return board[2];
+        }
+
+        return -1;
+    }
+
+    private int ComputerMove(int WhoseTurn)
+    {
+        for (int i = 0; i < _data.Length; i++)
+        {
+            int[] dataCopy = (int[])_data.Clone();
+            if (dataCopy[i] == -1)
+            {
+                PutValue(i, (WhoseTurn == 1 ? 0 : 1), dataCopy);
+                if (CheckWinCondition(dataCopy) == (WhoseTurn == 1 ? 0 : 1))
+                    return i;
+            }
+
+            dataCopy = (int[])_data.Clone();
+
+            if (dataCopy[i] == -1)
+            {
+                PutValue(i, WhoseTurn, dataCopy);
+                if (CheckWinCondition(dataCopy) == WhoseTurn)
+                    return i;
+            }
+        }
+
+        // Try taking the corners
+        Random rand = new Random();
+        List<int> Corners = new List<int> { 0, 2, 6, 8 };
+        do
+        {
+            int j = rand.Next(Corners.Count);
+            if (_data[Corners[j]] == -1)
+                return Corners[j];
+            else
+                Corners.RemoveAt(j);
+        } while (Corners.Count == 0);
+
+        // Take the middle
+        if (_data[4] == -1)
+            return 4;
+
+        List<int> sides = new List<int> { 1, 3, 5, 7 };
+        do
+        {
+            int j = rand.Next(sides.Count);
+            if (_data[sides[j]] == -1)
+                return sides[j];
+            else
+                sides.RemoveAt(j);
+        } while (sides.Count == 0);
+
+        return -1;
+    }
+
+    private void ShowMenu(int XorO)
+    {
+        Console.Write("Put a number here to put an " + XorYFromInt(XorO) + " there: ");
+    }
+
+    private bool PutValue(int at, int what, int[] array)
+    {
+        if (array[at] == -1)
+        {
+            array[at] = what;
+            return true;
+        }
+        else
+            return false;
+    }
+
+    private void DrawBoard()
+    {
+        Console.WriteLine("|-" + XorYFromInt(_data[0]) + "-|-" + XorYFromInt(_data[1]) + "-|-" + XorYFromInt(_data[2]) + "-|");
+        Console.WriteLine("|-" + XorYFromInt(_data[3]) + "-|-" + XorYFromInt(_data[4]) + "-|-" + XorYFromInt(_data[5]) + "-|");
+        Console.WriteLine("|-" + XorYFromInt(_data[6]) + "-|-" + XorYFromInt(_data[7]) + "-|-" + XorYFromInt(_data[8]) + "-|");
+    }
+
+    private string XorYFromInt(int number)
+    {
+        switch (number)
+        {
+            case 0:
+                return "X";
+
+            case 1:
+                return "O";
+
+            default:
+                return "-";
+        }
+    }
+}
--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,21 +1,15 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace TicTacToe
+public class Program
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
-        {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
-        }
+        Game game = new Game();
+        Console.WriteLine("Welcome To Tic Tac Toe Game!");
+        Console.Write("Press Enter to start the game");
+        Console.ReadLine();
+        game.start();
+        Console.ReadLine();
     }
 }
